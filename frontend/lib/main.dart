b897c2c54d833987937f0a49e05a848536c84d5e
Alessandro Masone <masonealessandro04@gmail.com>
2025-11-28 18:11:40 +0100

    feat(android): configura Firebase e Google Sign-In
    - Aggiunto google-services.json in android/app
    - Aggiornato settings.gradle.kts con il plugin google-services
    - Applicato plugin google-services in app/build.gradle.kts
    - Inizializzato Firebase e binding Flutter in main.dart"

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/providers/notification_provider.dart';
 import 'package:provider/provider.dart';
+// 1. AGGIUNGI QUESTO IMPORT
+import 'package:firebase_core/firebase_core.dart';
 
 // IMPORT DEI PROVIDER
 import 'package:frontend/providers/auth_provider.dart';
 import 'package:frontend/providers/medical_provider.dart';
 import 'package:frontend/providers/emergency_provider.dart';
-import 'package:frontend/providers/permission_provider.dart'; // <--- ASSICURATI DI QUESTO IMPORT
+import 'package:frontend/providers/permission_provider.dart';
 import 'package:frontend/ui/screens/auth/loading_screen.dart';
 
-void main() {
+// 2. AGGIUNGI 'async' QUI
+void main() async {
+  // 3. AGGIUNGI QUESTA RIGA (Obbligatoria per usare plugin prima di runApp)
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 4. INIZIALIZZA FIREBASE
+  await Firebase.initializeApp();
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
         ChangeNotifierProvider(create: (_) => MedicalProvider()),
         ChangeNotifierProvider(create: (_) => EmergencyProvider()),
-        ChangeNotifierProvider(create: (_) => PermissionProvider()), // <--- E DI QUESTA RIGA
+        ChangeNotifierProvider(create: (_) => PermissionProvider()),
         ChangeNotifierProvider(create: (_) => NotificationProvider()),
       ],
       child: const SAfeGuard(),
